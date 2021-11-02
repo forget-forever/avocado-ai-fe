99d10e90169cf805f789cb1ab71667018164f130
周美磊 <zhoumeilei@sfmail.sf-express.com>
2021-11-02 10:14:08 +0800

    引入阿里字体图标

--- src/pages/my/bindPhone/util.ts
+++ src/pages/my/bindPhone/util.ts
@@ -1,4 +1,4 @@
-import { isPhone } from "@/utils/utils"
+import { isPhone } from "@/utils/tool"
 
 export const canSubmit = (msg: IRequest.User.VerifyCodeBindParams) => {
   return isPhone(msg.phone) && msg.verifyCode?.length >= 3

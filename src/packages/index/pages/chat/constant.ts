eeeb9a05b1ad0254e3b30aa12142f38c5554d9e0
forget <hehezml@qq.com>
2023-04-28 23:31:32 +0800

    feat: 增加角色显示

--- src/packages/index/pages/chat/constant.ts
+++ src/packages/index/pages/chat/constant.ts
@@ -1,3 +1,67 @@
 export const CHAT_TITLE = '智能机器人';
 export const CHAT_ING_TITLE = '对方正在输入中...';
 export const MAX_INPUT_NUMBER = 1000;
+
+
+const messages = [
+  {
+    id: 32359,
+    userName: "hehezml",
+    name: "hahazml",
+    header: "https://aiquyin-static-beijing.oss-cn-beijing.aliyuncs.com/ChatGPT/Headers/boy1.png?x-oss-process=style/jmms",
+    conversationId: "768639cf-05d9-4387-9955-a4769b36f182",
+    messageId: "f518dd39-751a-482b-91af-546e4e1fd736",
+    isChatGpt: false,
+    content: "css怎么样, v发热我国北方天宝你很讨厌热闹就有黑幕剧院门口i",
+    index: 0,
+    createTime: new Date(),
+    status: 3,
+    city: "北京市",
+    checkStatus: 0
+  },
+  {
+    id: 32360,
+    userName: "hehezml",
+    name: '',
+    header: '',
+    conversationId: "768639cf-05d9-4387-9955-a4769b36f182",
+    messageId: "1c71fe4b-99f2-4f36-8ce9-54d59410374c",
+    isChatGpt: true,
+    content: "CSS（Cascading Style Sheets）是一种用于描述HTML（Hyper Text Markup Language）和XML（eXtensible Markup Language）文档外观和样式的语言。它用于控制网页的布局、字体、颜色、背景、边框、动画等样式。\n\nCSS的语法比较简单，可以通过选择器来定位HTML或XML中的元素，然后通过属性来改变元素的样式。CSS可以通过内部样式表、外部样式表或内联样式表的方式应用到HTML或XML文档中。\n\nCSS具有很多优点，例如：\n\n1. 可以将样式从HTML或XML文档中分离出来，使文档结构更加清晰，易于维护和修改。\n\n2. 可以通过继承、层叠等机制来优化样式的定义，减少代码量。\n\n3. 可以通过响应式设计来适应不同设备的屏幕尺寸和分辨率，提高网页的可访问性和用户体验。\n\n总之，CSS是网页设计中不可或缺的一部分，掌握CSS对于网页设计和开发非常重要。",
+    index: 1,
+    createTime: new Date,
+    status: 3,
+    city: '',
+    checkStatus: 0
+  },
+  {
+    id: 32365,
+    userName: "hehezml",
+    name: "hahazml",
+    header: "https://aiquyin-static-beijing.oss-cn-beijing.aliyuncs.com/ChatGPT/Headers/boy1.png?x-oss-process=style/jmms",
+    conversationId: "768639cf-05d9-4387-9955-a4769b36f182",
+    messageId: "d4a80019-33da-4db1-9eec-3f69a5539181",
+    isChatGpt: false,
+    content: "那它有啥新特性",
+    index: 2,
+    createTime: new Date(),
+    status: 3,
+    city: "北京市",
+    checkStatus: 0
+  },
+  {
+    id: 32366,
+    userName: "hehezml",
+    name: '',
+    header: '',
+    conversationId: "768639cf-05d9-4387-9955-a4769b36f182",
+    messageId: "80072087-42e9-416a-a631-3427b47c9135",
+    isChatGpt: true,
+    content: "CSS3是CSS的最新版本，其中包含了许多新特性。以下是CSS3的一些新特性：\n\n1. 选择器：CSS3引入了一些新的选择器，如属性选择器、伪类选择器、伪元素选择器等，使开发者能够更精细地定位元素。\n\n2. 盒子模型：CSS3中的盒子模型允许开发者精确地控制元素的尺寸和边距，包括设置元素的圆角、阴影等效果。\n\n3. 边框：CSS3新增了一些边框样式，如圆角边框、渐变边框、图像边框等，使得开发者能够更加灵活地设计元素的边框。\n\n4. 背景：CSS3中的背景属性允许开发者设置复杂的背景效果，如渐变背景、图像背景、多重背景等。\n\n5. 动画和过渡：CSS3中新增了一些动画和过渡效果，使得开发者能够为元素添加更加生动的效果，如旋转、缩放、淡入淡出等。\n\n6. 响应式设计：CSS3使得响应式设计更加容易实现，能够根据不同设备的屏幕尺寸和分辨率来自动调整样式。\n\n总之，CSS3的新特性让开发者能够更加灵活地定制网页的样式和效果，提高网页的质量和用户体验。",
+    index: 3,
+    createTime: new Date("2023-04-26T21:55:14.103"),
+    status: 3,
+    city: '',
+    checkStatus: 0
+  }
+]

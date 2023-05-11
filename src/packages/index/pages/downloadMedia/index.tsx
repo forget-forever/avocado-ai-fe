c86b8d9bd95e609de02aacb1d072e7adab6ddee3
forget <hehezml@qq.com>
2023-05-11 22:57:31 +0800

    feat: 调整一些宽度

--- src/packages/index/pages/downloadMedia/index.tsx
+++ src/packages/index/pages/downloadMedia/index.tsx
@@ -28,10 +28,10 @@ const DownloadMedia: React.FC = () => {
         <AtTextarea value='' onChange={invalidFunc} placeholder='平台视频分享文案（包括分享链接）' maxLength={200} />
       ) }
     </Card>
-    <View className='margin-top width-8'>
+    <View className='margin-top width-9 text-gray text-sm'>
       <Text>抖音 快手 小红书 微博 微视 皮皮虾 陌陌 唱吧 西瓜视频 今日头条 好看视频 全民小视频 看点视频 趣头条 全民K歌 酷狗音乐 酷我音乐 看看视频 梨视频 哔哩哔哩 网易云音乐 看点视频 QQ看点 小咖秀 看点快报 糖豆 配音秀 大众点评 懂车帝 火山 皮皮搞笑 最左 小影 火山小视频 轻视频 百度视频 新片场 迅雷 美图秀秀 秒拍 美拍 刷宝 剪影 京东 淘宝 天猫 微信公众号 虎牙视频 uc浏览器 QQ浏览器 oppo浏览器 油果浏览器 万能钥匙WiFi 知乎 腾讯新闻 人民日报 开眼 微叭 微云 快看点 彩视 TikTok youtube twitter VUE vigo ACfun yy now 等等200多个短视频平台。如果遇到提取失败请</Text>
     </View>
-    <ButtonAsync onClick={submit} type='primary'>提交无水印内容，需消耗{getContentWithoutWatermark}<Logo /></ButtonAsync>
+    <ButtonAsync onClick={submit} type='primary' className='margin-top width-8'>提交无水印内容，需消耗{getContentWithoutWatermark}<Logo /></ButtonAsync>
   </PageContainer>
 }
 export default DownloadMedia;
